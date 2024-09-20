acc13ea03f710f4716eb2f82c95c5edb49062a88
Lizeth Bejarano <l.bejarano1@uniandes.edu.co>
2024-09-20 11:16:23 -0500

    Creacion ClaseCocinaEntity

---
+++ src/main/java/co/edu/uniandes/dse/parcialprueba/entities/ClaseCocinaEntity.java
@@ -0,0 +1,14 @@
+package co.edu.uniandes.dse.parcialprueba.entities;
+
+import java.util.Date;
+
+public class ClaseCocinaEntity extends BaseEntity{
+
+    private String name;
+	private String description;
+	private String realizationDate;
+    private String duracion;
+    private String nivelDificultad;
+    private String cookingStyle;
+    private Long Id;
+}
