e2af3fd199c6679df5ac51eb5e7ec68e08e5909d
Lizeth Bejarano <l.bejarano1@uniandes.edu.co>
2024-09-20 11:33:21 -0500

    Correccion clase cocina entity y relacion clase cocina entity

--- src/main/java/co/edu/uniandes/dse/parcialprueba/entities/ClaseCocinaEntity.java
+++ src/main/java/co/edu/uniandes/dse/parcialprueba/entities/ClaseCocinaEntity.java
@@ -1,12 +1,29 @@
 package co.edu.uniandes.dse.parcialprueba.entities;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
+
+@Data
+@Entity
 
 public class ClaseCocinaEntity extends BaseEntity{
 
+    @PodamExclude
+    @ManyToMany
+    private List<ChefEntity> chefs = new ArrayList<>();
+
+    @Temporal(TemporalType.DATE)
+    private String realizationDate;
+
     private String name;
 	private String description;
-	private String realizationDate;
     private String duracion;
     private String nivelDificultad;
     private String cookingStyle;
