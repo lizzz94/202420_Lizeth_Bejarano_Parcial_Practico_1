17a345830c44f4cd1d71629ffad5b230ec4d263f
Lizeth Bejarano <l.bejarano1@uniandes.edu.co>
2024-09-20 11:43:18 -0500

    Creacion repositorio ClaseCocinaRepository

---
+++ src/main/java/co/edu/uniandes/dse/parcialprueba/repositories/ClaseCocinaRepository.java
@@ -0,0 +1,12 @@
+package co.edu.uniandes.dse.parcialprueba.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import co.edu.uniandes.dse.parcialprueba.entities.ClaseCocinaEntity;
+
+@Repository
+public interface ClaseCocinaRepository extends JpaRepository<ClaseCocinaEntity, Long> {
+
+
+}
